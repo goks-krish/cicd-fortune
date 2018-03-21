bad74fbcba73050b046163902e325a3b31d8004c
Ramachandran Gokulakrishnan <gokul.ramachandran@nagra.com>
2018-03-21 16:27:44 +0530

    Added code for create cluster
    Signed-off-by: Gokul<gokskrish@gmail.com>

--- deploy-pipeline.groovy
+++ deploy-pipeline.groovy
@@ -9,6 +9,9 @@ node() {
 
     stage('Create Cluster on GKE') { 
         echo "2. Create Cluster on GKE"
+        sh 'gcloud beta container --project "rock-loop-418" clusters create "cluster-fortune" --zone "asia-south1-c" --username "admin" --cluster-version "1.8.8-gke.0" --machine-type "f1-micro" --image-type "COS" --disk-size "100" --scopes "https://www.googleapis.com/auth/compute","https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --num-nodes "3" --network "default" --enable-cloud-logging --enable-cloud-monitoring --subnetwork "default"'
+        echo " Get credentials"
+        sh 'gcloud container clusters get-credentials cluster-fortune --zone asia-south1-c --project rock-loop-418'
     }
 
     stage('Deploy on Kubernetes') { 
